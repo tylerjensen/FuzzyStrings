25e4845e4bf5fca73cc8190f56b34901b2431694
13xforever <ilya.veselov@gmail.com>
2021-03-03 02:03:09 +0500

    Replace DiceCoefficient implementation
    Removes input string padding as it introduces bias for the first and last character matches.
    As a consequence, comparing with one-letter string will return score of 0 as it has zero bigrams now.
    
    Fixes #8

--- src/DuoVia.FuzzyStrings/DuoVia.FuzzyStrings/DiceCoefficientExtensions.cs
+++ src/DuoVia.FuzzyStrings/DuoVia.FuzzyStrings/DiceCoefficientExtensions.cs
@@ -20,9 +20,15 @@ namespace DuoVia.FuzzyStrings
 		/// <returns></returns>
 		public static double DiceCoefficient(this string input, string comparedTo)
 		{
-			var ngrams = input.ToBiGrams();
-			var compareToNgrams = comparedTo.ToBiGrams();
-			return ngrams.DiceCoefficient(compareToNgrams);
+			if (input == comparedTo)
+				return 1.0d;
+
+			if (input.Length < 2 || comparedTo.Length < 2)
+				return 0.0d;
+
+			var biGrams = input.ToBiGrams(false);
+			var compareToBiGrams = comparedTo.ToBiGrams(false);
+			return DiceCoefficient(biGrams, compareToBiGrams);
 		}
 
 		/// <summary>
@@ -33,27 +39,57 @@ namespace DuoVia.FuzzyStrings
 		/// <returns></returns>
 		public static double DiceCoefficient(this string[] nGrams, string[] compareToNGrams)
 		{
-			int matches = nGrams.Intersect(compareToNGrams).Count();
-			if (matches == 0) return 0.0d;
+			var nGramMap = new Dictionary<string, int>(nGrams.Length);
+			var compareToNGramMap = new Dictionary<string, int>(compareToNGrams.Length);
+			var nGramSet = new HashSet<string>();
+			var compareToNGramSet = new HashSet<string>();
+			foreach (var nGram in nGrams)
+			{
+				if (nGramSet.Add(nGram))
+					nGramMap[nGram] = 1;
+				else
+					nGramMap[nGram]++;
+			}
+			foreach (var nGram in compareToNGrams)
+			{
+				if (compareToNGramSet.Add(nGram))
+					compareToNGramMap[nGram] = 1;
+				else
+					compareToNGramMap[nGram]++;
+			}
+			nGramSet.IntersectWith(compareToNGramSet);
+			if (nGramSet.Count == 0)
+				return 0.0d;
+
+			var matches = 0;
+			foreach (var nGram in nGramSet)
+				matches += Math.Min(nGramMap[nGram], compareToNGramMap[nGram]);
+			
 			double totalBigrams = nGrams.Length + compareToNGrams.Length;
 			return (2 * matches) / totalBigrams;
 		}
 
-		public static string[] ToBiGrams(this string input)
+		public static string[] ToBiGrams(this string input, bool usePadding = true)
 		{
-			// nLength == 2
-			//   from Jackson, return %j ja ac ck ks so on n#
-			//   from Main, return #m ma ai in n#
-			input = SinglePercent + input + SinglePound;
+			if (usePadding)
+			{
+				// nLength == 2
+				//   from Jackson, return %j ja ac ck ks so on n#
+				//   from Main, return %m ma ai in n#
+				input = SinglePercent + input + SinglePound;
+			}
+			if (input.Length < 2)
+				return new string[0];
+			
 			return ToNGrams(input, 2);
 		}
 
 		public static string[] ToTriGrams(this string input)
 		{
 			// nLength == 3
-			//   from Jackson, return %%j %ja jac ack cks kso son on# n##
-			//   from Main, return ##m #ma mai ain in# n##
-			input = DoublePercent + input + DoublePount;
+			//   from Jackson, return &&j &ja jac ack cks kso son on# n##
+			//   from Main, return &&m &ma mai ain in# n##
+			input = DoubleAmpersand + input + DoublePound;
 			return ToNGrams(input, 3);
 		}
 
@@ -61,13 +97,14 @@ namespace DuoVia.FuzzyStrings
 		{
 			int itemsCount = input.Length - 1;
 			string[] ngrams = new string[input.Length - 1];
-			for (int i = 0; i < itemsCount; i++) ngrams[i] = input.Substring(i, nLength);
+			for (int i = 0; i < itemsCount; i++)
+				ngrams[i] = input.Substring(i, nLength);
 			return ngrams;
 		}
 
 		private const string SinglePercent = "%";
 		private const string SinglePound = "#";
-		private const string DoublePercent = "&&";
-		private const string DoublePount = "##";
+		private const string DoubleAmpersand = "&&";
+		private const string DoublePound = "##";
 	}
 }
